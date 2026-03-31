32cf51c735b8ae30e02606437e2833a379b64a01
Aarnava <aarnavarajan0603@gmail.com>
2026-03-31 22:10:54 +0530

    added subsystems pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./styles/App.css";
-import MSsec from "./pages/MSsec";
+import Subsystems from "./pages/Subsystems";
+import SubsystemDetail from "./pages/SubsystemDetail";
 
 export default function App() {
-  // get request from backend
-  // const mentors = [
-  //   {
-  //     id: 1,
-  //     profileUrl: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
-  //     name: "XYZ",
-  //     sys: "Avionics",
-  //     des: "...",
-  //     email: "xyz@gmail.com",
-  //     linkedin: "xyz",
-  //   },
-  //   {
-  //     id: 2,
-  //     profileUrl: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
-  //     name: "Jane Doe",
-  //     sys: "Software Engineer",
-  //     des: "...",
-  //     email: "jane@gmail.com",
-  //     linkedin: "Jane",
-  //   },
-  // ];
-
-  // const sponsors = [
-  //   {
-  //     name: "Sponsor One",
-  //     logoUrl:
-  //       "https://via.placeholder.com/150x80/0000FF/FFFFFF?text=Sponsor+1",
-  //   },
-  //   {
-  //     name: "Sponsor Two",
-  //     logoUrl:
-  //       "https://via.placeholder.com/150x80/FF0000/FFFFFF?text=Sponsor+2",
-  //   },
-  //   {
-  //     name: "Sponsor Three",
-  //     logoUrl:
-  //       "https://via.placeholder.com/150x80/00FF00/FFFFFF?text=Sponsor+3",
-  //   },
-  // ];
-
-  // return <MSsec mentorList={mentors} sponsors={sponsors} />;
-
-
+  return (
+    <Routes>
+      <Route path="/" element={<Subsystems />} />
+      <Route path="/subsystems/:id" element={<SubsystemDetail />} />
+    </Routes>
+  );
 }
-
-
