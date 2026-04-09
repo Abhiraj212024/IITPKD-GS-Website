856eccc9f864df7df4b476f6c8a96191ed8e3dcd
Abhiraj212024 <abhirajraje1@gmail.com>
2026-04-09 16:38:56 +0530

    updated vite config for SERVER PORT

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // This tells Vite: "If a request starts with /member, send it to the backend"
+      '/member': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
